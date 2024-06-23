153350e16428b1457b5f9baeff2881fd3942c8cb
Deepak K V <deepak.k@skillassure.com>
2024-06-23 17:01:52 +0530

    contact page's first section finished

--- src/components/pages/dynamic/contact/Contact.jsx
+++ src/components/pages/dynamic/contact/Contact.jsx
@@ -1,22 +1,11 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { Icon } from '@iconify-icon/react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { } from '@fortawesome/free-solid-svg-icons';
 
 
-const bio = [
-    "Deepak K.V",
-    "+91 9947 9180 68",
-    "kvdeepakkv1995@gmail.com",
-    "deepudeepz383@gmail.com",
-    "Ottapalam , Kerala",
-]
 
-function Bioprint(props) {
-    return <li className='contact-li'>{props.name}</li>
-}
 const Contact = () => {
     const form = useRef();
 
@@ -41,68 +30,70 @@ const Contact = () => {
 
     return (
         <div className='contact-section'>
-            <div className='home-background'>
-                <div className='box'>
-                    <div className='wave -one'></div>
-                    <div className='wave -two'></div>
-                    <div className='wave -three'></div>
-                </div>
-            </div>
-
-
-            <div className="container-contact">
-                <div className="row">
-                    <h4 className="cnt-title" style={{ textAlign: 'center' }}>Re<span className='greenish'>a</span>ch Me!</h4>
-                </div>
-                <div className="input-container">
-                    <form ref={form} onSubmit={sendEmail}>
-                        <div className="c2">
-                            <div className="styled-input wide">
-                                <input type="text" name="user_name" required />
-                                <label>Name</label>
-                            </div>
-                        </div>
-                        <div className="c2 c3" >
-                            <div className="styled-input">
-                                <input type="email" name="user_email" required />
-                                <label>Email</label>
-                            </div>
-                            <div className="styled-input" style={{ float: 'right' }}>
-                                <input type="text" name="user_phone" required />
-                                <label>Phone Number</label>
-                            </div>
-                        </div>
-                        <div className="c2">
-                            <div className="styled-input wide">
-                                <textarea name="message" required></textarea>
-                                <label>Message</label>
-                            </div>
-                        </div>
-                        <div className="c2">
-                            <input type="submit" value="Send" className="btn-lrg submit-btn" />
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            <div className='contact-footer'>
-                <div className='contact-details-section'>
-                    <ul className='contact-ul'>
-                        {bio.map((det) => <Bioprint name={det} />)}
-                    </ul>
-                </div>
-                <div className='contact-divider'></div>
-                <div className='contact-web-details'>
-                    <div className='gitdetails'>
-                        <a href="https://github.com/deepuvkrish" target="_blank" rel="noopener noreferrer">Github</a>
-                        <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff", }} />
+            <div className='contact-intro'>
+                <div className='contact-intro-box'>
+                    <div className='contact-intro-box-left'>
+                        <h1>Get in touch.</h1>
                     </div>
-                    <div className='linkeddetails'>
-                        <a href="https://www.linkedin.com/in/deepak-k-v-b51752161/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                        <FontAwesomeIcon icon={faLinkedin} style={{ color: "#ffffff", }} />
+                    <div className='contact-intro-box-right'>
+                        <div className='contact-address cnt'>
+                            <div className='contact-icon'>
+                                <Icon icon="material-symbols:home-pin-outline" width="3em" height="3em" />
+                            </div>
+                            <div className='details'>
+                                <span className='cnthead'> Address</span>
+                                <span>Ottapalam, Kerala</span>
+                            </div>
+                        </div>
+                        <div className='contact-phone cnt'>
+                            <div className='contact-icon'>
+                                <Icon icon="game-icons:phone" width="3em" height="3em" />
+                            </div>
+                            <div className='details'>
+                                <span className='cnthead'> Phone</span>
+                                <span>+91 994 791 8068</span>
+                            </div>
+                        </div>
+
+                        <div className='contact-mail cnt'>
+                            <div className='contact-icon'>
+                                <Icon icon="emojione-monotone:e-mail" width="3em" height="3em" />
+                            </div>
+                            <div className='details'>
+                                <span className='cnthead'>Mail</span>
+                                <span>deepudeepz383@gmail.com</span>
+
+                            </div>
+
+                        </div>
+                        <div className='contact-network cnt'>
+                            <div className='contact-icon'>
+                                <Icon icon="gridicons:reader-follow-conversation" width="3em" height="3em" />
+                            </div>
+                            <div className='details'>
+                                <span className='cnthead'>Follow me</span>
+
+                                <div className='contact-network-links'>
+                                    <span><Icon icon="entypo-social:linkedin-with-circle" width="2.5em" height="2.5em" /></span>
+                                    <span><Icon icon="ri:github-fill" width="2.5em" height="2.5em" /></span>
+                                    <span><Icon icon="mage:instagram-circle" width="2.5em" height="2.5em" /></span>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
+
+                </div>
+
+            </div>
+            <div className='contact-form'>
+                <div className='contact-form-box'>
+
+                    <h1 style={{ fontSize: '150px' }}>Coming Soon</h1>
                 </div>
             </div>
+
+
 
         </div>
 
---
+++ src/sample.jsx
@@ -0,0 +1,202 @@
+//    <div className="container-contact">
+//                 <div className="row">
+//                     <h4 className="cnt-title" style={{ textAlign: 'center' }}>Re<span className='greenish'>a</span>ch Me!</h4>
+//                 </div>
+//                 <div className="input-container">
+//                     <form ref={form} onSubmit={sendEmail}>
+//                         <div className="c2">
+//                             <div className="styled-input wide">
+//                                 <input type="text" name="user_name" required />
+//                                 <label>Name</label>
+//                             </div>
+//                         </div>
+//                         <div className="c2 c3" >
+//                             <div className="styled-input">
+//                                 <input type="email" name="user_email" required />
+//                                 <label>Email</label>
+//                             </div>
+//                             <div className="styled-input" style={{ float: 'right' }}>
+//                                 <input type="text" name="user_phone" required />
+//                                 <label>Phone Number</label>
+//                             </div>
+//                         </div>
+//                         <div className="c2">
+//                             <div className="styled-input wide">
+//                                 <textarea name="message" required></textarea>
+//                                 <label>Message</label>
+//                             </div>
+//                         </div>
+//                         <div className="c2">
+//                             <input type="submit" value="Send" className="btn-lrg submit-btn" />
+//                         </div>
+//                     </form>
+//                 </div>
+//             </div>
+
+
+
+//             <div className='contact-footer'>
+//                 <div className='contact-details-section'>
+//                     <ul className='contact-ul'>
+//                         {bio.map((det) => <Bioprint name={det} />)}
+//                     </ul>
+//                 </div>
+//                 <div className='contact-divider'></div>
+//                 <div className='contact-web-details'>
+//                     <div className='gitdetails'>
+//                         <a href="https://github.com/deepuvkrish" target="_blank" rel="noopener noreferrer">Github</a>
+//                         <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff", }} />
+//                     </div>
+//                     <div className='linkeddetails'>
+//                         <a href="https://www.linkedin.com/in/deepak-k-v-b51752161/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+//                         <FontAwesomeIcon icon={faLinkedin} style={{ color: "#ffffff", }} />
+//                     </div>
+//                 </div>
+//             </div>
+
+
+
+
+
+// <div className='home-background'>
+//                 <div className='box'>
+//                     <div className='wave -one'></div>
+//                     <div className='wave -two'></div>
+//                     <div className='wave -three'></div>
+//                 </div>
+//             </div>
+
+
+
+
+
+
+
+
+// .cnt - title{
+//     text - transform: capitalize;
+//     font - size: 28px;
+// }
+//   .container - contact{
+//     margin - bottom: 50px;
+//     position: relative;
+//     z - index: 999;
+// }
+//   .contact - footer{
+//     width: 100 %;
+//     display: flex;
+//     flex - direction: row;
+//     position: relative;
+//     overflow: hidden;
+// }
+//   .contact - li{
+//     text - transform: capitalize;
+//     margin - block: 10px;
+//     list - style: none;
+// }
+//   .contact - details - section, .contact - web - details{
+//     width: 50 %;
+//     position: relative;
+//     z - index: 10;
+// }
+//   .contact - web - details{
+//     display: flex;
+//     flex - direction: column;
+//     height: 200px;
+//     justify - content: center;
+//     align - items: center;
+// }
+//   .contact - web - details a:hover{
+//     letter - spacing: 2px;
+//     transition: 1s;
+// }
+//   .contact - divider{
+//     width: 80 %;
+//     height: 224px;
+//     background: var(--greenText);
+//     position: absolute;
+//     transform: skewX(317deg);
+//     top: -6 %;
+//     left: 43 %;
+//     z - index: 5;
+// }
+//   .contact - section{
+//     height: 100 %;
+//     display: flex;
+//     flex - direction: column;
+// }
+
+// input:focus ~label, textarea:focus ~label, input:valid ~label, textarea:valid ~label {
+//     font - size: 0.75em;
+//     color: #999;
+//     top: -5px;
+//     -webkit - transition: all 0.225s ease;
+//     transition: all 0.225s ease;
+// }
+  
+//   .styled - input {
+//     float: left;
+//     width: 293px;
+//     margin: 1rem 0;
+//     position: relative;
+//     border - radius: 4px;
+// }
+//   .styled - input label {
+//     color: #999;
+//     padding: 1.3rem 30px 1rem 30px;
+//     position: absolute;
+//     top: 10px;
+//     left: 0;
+//     -webkit - transition: all 0.25s ease;
+//     transition: all 0.25s ease;
+//     pointer - events: none;
+// }
+//   .styled - input.wide {
+//     width: 650px;
+//     max - width: 100 %;
+// }
+// input,
+//     textarea {
+//     padding: 30px;
+//     border: 0;
+//     width: 100 %;
+//     font - size: 1rem;
+//     background - color: #2d2d2d;
+//     color: white;
+//     border - radius: 4px;
+// }
+// input: focus,
+//     textarea:focus { outline: 0; }
+// input:focus ~span,
+//     textarea:focus ~span {
+//     width: 100 %;
+//     -webkit - transition: all 0.075s ease;
+//     transition: all 0.075s ease;
+// }
+//   textarea {
+//     width: 100 %;
+//     min - height: 15em;
+// }
+//   .input - container {
+//     width: 650px;
+//     max - width: 100 %;
+//     margin: 20px auto 25px auto;
+// }
+//   .submit - btn {
+//     padding: 7px 35px;
+//     border - radius: 60px;
+//     display: inline - block;
+//     background - color: var(--greenText);
+//     color: white;
+//     font - size: 18px;
+//     cursor: pointer;
+//     box - shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.06),
+//         0 2px 10px 0 rgba(0, 0, 0, 0.07);
+//     -webkit - transition: all 300ms ease;
+//     transition: all 300ms ease;
+// }
+//   .submit - btn:hover {
+//     transform: translateY(1px);
+//     box - shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.10),
+//         0 1px 1px 0 rgba(0, 0, 0, 0.09);
+// }
