57c1c678409ad91854f46c3bb5fd1c524aa12b37
Deepak K V <deepak.k@skillassure.com>
2024-07-02 17:19:17 +0530

    nav responsive added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import React from "react";
-import { useEffect, useMemo, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
-
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -25,138 +21,37 @@ import Lib from './components/pages/dynamic/projects/minip/Lib';
 
 
 function App() {
-  const [init, setInit] = useState(false);
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      await loadSlim(engine);
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+  return (
+    <div className="App">
+      <HelmetProvider>
+        <Helmet>
+          <html lang="en" />
+          <title>Deepak KV</title>
+          <meta name="description" content="Tutorial for React Helmet" />
+          <meta name="theme-color" content="#E6E6FA" />
+        </Helmet>
+      </HelmetProvider>
 
-
-  const options = useMemo(
-    () => ({
-      background: {
-        color: {
-          value: "#000",
-        },
-      },
-      pauseOnOutsideViewport: true,
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 40,
-          },
-          repulse: {
-            distance: 150,
-            duration: 0.4,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: "#ffffff5c",
-        },
-        links: {
-          color: "#ffffff5c",
-          distance: 150,
-          enable: true,
-          opacity: 0.1,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "destroy",
-          },
-          random: true,
-          speed: 3,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 0.1,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
-      },
-      detectRetina: true,
-    }),
-    [],
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/career' element={<Career />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/skillassure' element={<SkillAssure />} />
+            <Route path='/nadaar' element={<Nadaar />} />
+            <Route path='/padippi' element={<Padippi />} />
+            <Route path='/brain' element={<Brain />} />
+            <Route path='/lib' element={<Lib />} />
+            <Route path='/petzi' element={<Petzi />} />
+            <Route path='/campus' element={<Campus />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-  if (init) {
-    return (
-      <div className="App">
-        <Particles
-          id="tsparticles"
-          particlesLoaded={particlesLoaded}
-          options={options}
-        />
-        {/* <MouseTrail strokeColor={"#b9e313"} lineDuration={5} lineWidthStart={5} number={1} /> */}
-
-        <HelmetProvider>
-          <Helmet>
-            <html lang="en" />
-            <title>Deepak KV</title>
-            <meta name="description" content="Tutorial for React Helmet" />
-            <meta name="theme-color" content="#E6E6FA" />
-          </Helmet>
-        </HelmetProvider>
-
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Navbar />}>
-              <Route index element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/career' element={<Career />} />
-              <Route path='/projects' element={<Projects />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/skillassure' element={<SkillAssure />} />
-              <Route path='/nadaar' element={<Nadaar />} />
-              <Route path='/padippi' element={<Padippi />} />
-              <Route path='/brain' element={<Brain />} />
-              <Route path='/lib' element={<Lib />} />
-              <Route path='/petzi' element={<Petzi />} />
-              <Route path='/campus' element={<Campus />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
-
-
-
-
-
-
-
-
 }
 
 export default App;
--- src/components/pages/static/Navbar.jsx
+++ src/components/pages/static/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import '../../../assets/css/Headfoot.css';
 
@@ -8,57 +8,155 @@ import { faHouse, faUserTie, faCommentDots } from '@fortawesome/free-solid-svg-i
 
 
 const Navbar = () => {
+    const [style, setStyle] = useState("navlinks-deactive");
+    const [spanfirst, setSpanFirst] = useState("spannav1");
+    const [spantwo, setSpanTwo] = useState("spannav2");
+    const [spanthree, setSpanThree] = useState("spannav3");
+    const navShow = () => {
+
+        if (style !== "navlinks-active") {
+            setStyle("navlinks-active");
+            setSpanFirst("spannav1-active");
+            setSpanTwo("spannav2-active");
+            setSpanThree("spannav3-active");
+        }
+        else {
+            setStyle("navlinks-deactive");
+            setSpanFirst("spannav1");
+            setSpanTwo("spannav2");
+            setSpanThree("spannav3");
+
+        }
+    };
+    const hideNav = () => {
+        if (style !== "navlinks-deactive") {
+            setStyle("navlinks-deactive");
+            setSpanFirst("spannav1");
+            setSpanTwo("spannav2");
+            setSpanThree("spannav3");
+        }
+        else {
+            setStyle("navlinks-active");
+            setSpanFirst("spannav1-active");
+            setSpanTwo("spannav2-active");
+            setSpanThree("spannav3-active");
+        }
+    }
+
     return (
-        <div className='navbar'>
-            <div className='navlinks'>
-                <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
-                    <div className="button-link">
-                        <div className="icon-link">
-                            <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff", }} />
-                        </div>
-                        <span>Home</span>
-                    </div>
-                </NavLink>
 
-                <NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
-                    <div className="button-link">
-                        <div className="icon-link">
-                            <FontAwesomeIcon icon={faUserTie} style={{ color: "#ffffff", }} />
+        <div className="nav-box">
+            <div className='navbar-laptop'>
+                <div className='navlinks'>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                        <div className="button-link">
+                            <div className="icon-link">
+                                <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff", }} />
+                            </div>
+                            <span>Home</span>
                         </div>
-                        <span>About</span>
-                    </div>
-                </NavLink>
-                <NavLink to='/projects' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
-                    <div className="button-link">
-                        <div className="icon-link">
-                            <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff", }} />
+                    </NavLink>
+
+                    <NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                        <div className="button-link">
+                            <div className="icon-link">
+                                <FontAwesomeIcon icon={faUserTie} style={{ color: "#ffffff", }} />
+                            </div>
+                            <span>About</span>
                         </div>
-                        <span>Projects</span>
-                    </div>
-                </NavLink>
-
-                <NavLink to='/career' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
-                    <div className="button-link">
-                        <div className="icon-link">
-                            <FontAwesomeIcon icon={faGoogleScholar} style={{ color: "#ffffff", }} />
+                    </NavLink>
+                    <NavLink to='/projects' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                        <div className="button-link">
+                            <div className="icon-link">
+                                <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff", }} />
+                            </div>
+                            <span>Projects</span>
                         </div>
-                        <span>Career</span>
-                    </div>
-                </NavLink>
+                    </NavLink>
 
-
-
-                <NavLink to='/contact' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
-                    <div className="button-link">
-                        <div className="icon-link">
-                            <FontAwesomeIcon icon={faCommentDots} style={{ color: "#ffffff", }} />
+                    <NavLink to='/career' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                        <div className="button-link">
+                            <div className="icon-link">
+                                <FontAwesomeIcon icon={faGoogleScholar} style={{ color: "#ffffff", }} />
+                            </div>
+                            <span>Career</span>
                         </div>
-                        <span>Contact</span>
-                    </div>
-                </NavLink>
+                    </NavLink>
+
+
+
+                    <NavLink to='/contact' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                        <div className="button-link">
+                            <div className="icon-link">
+                                <FontAwesomeIcon icon={faCommentDots} style={{ color: "#ffffff", }} />
+                            </div>
+                            <span>Contact</span>
+                        </div>
+                    </NavLink>
+                </div>
+
+                <Outlet />
             </div>
+            <div className='navbar-mobile'>
+                <div className='navbar-top'>
 
-            <Outlet />
+                </div>
+                <div className={style}>
+                    <div className='navbtn'>
+                        <button className='nav-icn' onClick={navShow} onMouseLeave={hideNav}>
+                            <span className={spanfirst}></span>
+                            <span className={spantwo}></span>
+                            <span className={spanthree}></span>
+                        </button>
+                    </div>
+                    <div className='nlinks'>
+                        <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                            <div className="button-link">
+                                <div className="icon-link">
+                                    <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff", }} />
+                                </div>
+                                <span>Home</span>
+                            </div>
+                        </NavLink>
+                        <NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                            <div className="button-link">
+                                <div className="icon-link">
+                                    <FontAwesomeIcon icon={faUserTie} style={{ color: "#ffffff", }} />
+                                </div>
+                                <span>About</span>
+                            </div>
+                        </NavLink>
+                        <NavLink to='/projects' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                            <div className="button-link">
+                                <div className="icon-link">
+                                    <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff", }} />
+                                </div>
+                                <span>Projects</span>
+                            </div>
+                        </NavLink>
+                        <NavLink to='/career' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                            <div className="button-link">
+                                <div className="icon-link">
+                                    <FontAwesomeIcon icon={faGoogleScholar} style={{ color: "#ffffff", }} />
+                                </div>
+                                <span>Career</span>
+                            </div>
+                        </NavLink>
+                        <NavLink to='/contact' className={({ isActive }) => (isActive ? 'active' : 'inactive')} >
+                            <div className="button-link">
+                                <div className="icon-link">
+                                    <FontAwesomeIcon icon={faCommentDots} style={{ color: "#ffffff", }} />
+                                </div>
+                                <span>Contact</span>
+                            </div>
+                        </NavLink>
+
+                    </div>
+
+                </div>
+
+                <Outlet />
+            </div>
         </div>
     )
 }
